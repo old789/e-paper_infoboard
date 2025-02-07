7c847a6880b04793a693700be2e1ecc06a1e6a6b
old789 <old789@users.noreply.github.com>
2025-02-07 18:09:19 +0200

    More works
    Receiving image data over WiFi mostly done

--- e-paper_infoboard.ino
+++ e-paper_infoboard.ino
@@ -1,49 +1,83 @@
+#define DBG_WIFI    // because "DEBUG_WIFI" defined in a WiFiClient library
+
 #include "DEV_Config.h"
 //#include "EPD.h"
 #include "utility/Debug.h"
 #ifndef __DEBUG_H
 #define Debug(__info)
 #endif
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <NetworkClientSecure.h>
 #include "utility/EPD_7in5_V2.h"
 #include "GUI_Paint.h"
-#include "ImageData.h"
+//#include "ImageData.h"
 #include <stdlib.h>
 
+#include "TickTwo.h"    // https://github.com/sstaub/TickTwo
+
+#include "config.h"
+
+UWORD Imagesize = ((EPD_7IN5_V2_WIDTH % 8 == 0) ? (EPD_7IN5_V2_WIDTH / 8 ) : (EPD_7IN5_V2_WIDTH / 8 + 1)) * EPD_7IN5_V2_HEIGHT;
 UBYTE *BlackImage;
+UBYTE *UsualImage;
+unsigned long curr_crc = 0;
+unsigned long prev_crc = 0;
+
+void check_updates();
+
+//TickTwo timer1( check_updates, 300000);
+TickTwo timer1( check_updates, 60000);
 
 void setup (){
   DEV_Module_Init();
   DEV_Delay_ms(200);
-  Debug("\r\n\r\nSerial debug started");
-
-  Debug("e-Paper Init and Clear...\r\m");
-  EPD_7IN5_V2_Init();
-  EPD_7IN5_V2_Clear();
-  DEV_Delay_ms(500);
-
-  UWORD Imagesize = ((EPD_7IN5_V2_WIDTH % 8 == 0) ? (EPD_7IN5_V2_WIDTH / 8 ) : (EPD_7IN5_V2_WIDTH / 8 + 1)) * EPD_7IN5_V2_HEIGHT;
+  Debug("\r\n\r\nSerial debug started\r\n");
+  wifi_init();
+  timer1.start();
   if ((BlackImage = (UBYTE *)malloc(Imagesize)) == NULL) {
-    Debug("Failed to apply for black memory...");
+    Debug("Failed to apply for black memory...\r\n");
+    while (1);
+  }
+  if ((UsualImage = (UBYTE *)malloc(Imagesize)) == NULL) {
+    Debug("Failed to apply memory for usual image...\r\n");
     while (1);
   }
   // Debug("Paint_NewImage\r\n");
   Paint_NewImage(BlackImage, EPD_7IN5_V2_WIDTH, EPD_7IN5_V2_HEIGHT, 0, WHITE);
-  //DEV_Delay_ms(5000);
+}
 
-  Debug("Paint_DrawBitMap\r\n");
-  Paint_DrawBitMap(pict);
-  //DEV_Delay_ms(5000);
+void loop () {
+  timer1.update();
+}
+
+void check_updates(){
+  Debug("Regular update\r\n");
+  bool rc = read_data();
+  if ( rc ) {
+    curr_crc = ram_crc();
+    Serial.printf("CRC = %u prev CRC = %u\r\n", curr_crc, prev_crc);
+    if ( curr_crc != prev_crc ) {
+      Debug("Need to redraw screen\r\n");
+      prev_crc = curr_crc;
+    // write_to_display();
+    }
+  }
+}
+
+void write_to_display (){
+
+  Debug("e-Paper Init and Clear...\r\n");
+  EPD_7IN5_V2_Init();
+  EPD_7IN5_V2_Clear();
+  DEV_Delay_ms(500);
 
   Debug("EPD_7IN5_V2_Display\r\n");
-  EPD_7IN5_V2_Display(BlackImage);
+  EPD_7IN5_V2_Display(UsualImage);
   DEV_Delay_ms(500);
 
   Debug("Goto Sleep...\r\n");
   EPD_7IN5_V2_Sleep();
   free(BlackImage);
   BlackImage = NULL;
-}
-
-void loop () {
-//
 }
---
+++ wifi.ino
@@ -0,0 +1,141 @@
+bool read_data(){
+
+  //Check WiFi connection status
+  if(WiFi.status() != WL_CONNECTED){
+#ifdef DBG_WIFI
+    Serial.println("WiFi Disconnected");
+#endif
+    wifi_init();
+  }
+
+#ifdef DBG_WIFI
+  Serial.println("Receive data");
+#endif
+
+  NetworkClientSecure *client = new NetworkClientSecure;
+  if (!client) {
+#ifdef DBG_WIFI
+    Serial.println("Unable to create client");
+#endif
+    return(false);
+  }
+  //client->setCACert(rootCACertificate);
+  client->setInsecure();
+
+  // Add a scoping block for HTTPClient https to make sure it is destroyed before NetworkClientSecure *client is
+  HTTPClient https;
+
+#ifdef DBG_WIFI
+  Serial.println("[HTTPS] begin...");
+#endif
+  if (!https.begin(*client, host, port, uri, true)) {  // HTTPS
+#ifdef DBG_WIFI
+    Serial.println("[HTTPS] Unable to connect");
+#endif
+    return(false);
+  }
+#ifdef DBG_WIFI
+  Serial.println("[HTTPS] GET...");
+#endif
+  // start connection and send HTTP header
+  int httpCode = https.GET();
+
+  // HTTP header has been send and Server response header has been handled
+#ifdef DBG_WIFI
+  Serial.printf("[HTTPS] GET... code: %d\r\n", httpCode);
+#endif
+  // httpCode will be negative on error
+  if (httpCode < 0) {
+    return(false);
+  }
+  // file found at server
+  if ( httpCode != HTTP_CODE_OK ) {
+#ifdef DBG_WIFI
+    Serial.printf("[HTTPS] GET... failed, error: %s\r\n", https.errorToString(httpCode).c_str());
+#endif
+    return(false);
+  }
+
+  // get length of document (is -1 when Server sends no Content-Length header)
+  int len = https.getSize();
+#ifdef DBG_WIFI
+  Serial.printf("[HTTPS] GET... size: %d\r\n", len);
+#endif
+
+  // create buffer for read
+  uint8_t buff[1024] = {0};
+  uint32_t j=0;
+
+  // get tcp stream
+  NetworkClient *stream = https.getStreamPtr();
+
+  // read all data from server
+  while (https.connected() && (len > 0 || len == -1)) {
+    // get available data size
+    size_t size = stream->available();
+
+    if (size) {
+      int c = stream->readBytes(buff, ((size > sizeof(buff)) ? sizeof(buff) : size));
+
+      if ( (j + c) <= Imagesize ) {
+        memcpy(UsualImage+j, buff, c);
+        j+=c;
+#ifdef DBG_WIFI
+        Serial.printf("[HTTPS] %d bytes copied, %d total\r\n", c, j);
+#endif
+      }
+
+      if (len > 0) {
+        len -= c;
+      }
+    }
+  }
+
+  https.end();
+
+  delete client;
+
+  return(true);
+}
+
+void wifi_init(){
+  uint16_t wifi_tries = 0;
+#ifdef DBG_WIFI
+  Serial.print("Connecting to "); Serial.print(ssid); Serial.println(" ...");
+#endif
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, passw);             // Connect to the network
+
+  uint16_t i = 0;
+  while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+    delay(100);
+    i++;
+#ifdef DBG_WIFI
+    Serial.print(i); Serial.print(' ');
+#endif
+    if ( i > 1500 ) {  // if don't connect - wait, then next try
+      if ( ++wifi_tries > 3 ) {
+        ESP.restart();
+      }
+      delay(150000);
+#ifdef DBG_WIFI
+      Serial.print("Try "); Serial.print(wifi_tries); Serial.println(" connect to WiFi");
+#endif
+    }
+  }
+
+  if ( wifi_tries != 0 ) {
+    wifi_tries=0;
+  }
+
+  WiFi.setAutoReconnect(true);
+  WiFi.persistent(true);
+
+#ifdef DBG_WIFI
+  Serial.println('\n');
+  Serial.println("Connection established!");
+  Serial.print("IP address: ");Serial.println(WiFi.localIP());
+  Serial.print("RSSI: ");Serial.println(WiFi.RSSI());
+#endif
+}
